55f893e273c22d3672ea4f46f032ec5f6cb1339f
zhe chen <wztxy89@163.com>
2022-11-19 20:32:11 +0800

    add --deterministic by default

--- segmentation/dist_train.sh
+++ segmentation/dist_train.sh
@@ -6,4 +6,4 @@ PORT=${PORT:-29300}
 
 #PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
 python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
-    $(dirname "$0")/train.py $CONFIG --launcher pytorch ${@:3}
+    $(dirname "$0")/train.py $CONFIG --launcher pytorch --deterministic ${@:3}
